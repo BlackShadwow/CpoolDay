fa87daa3e9eaa928d0087adb0818f05b7a5eeec2
BlackShadwow <41923687+BlackShadwow@users.noreply.github.com>
2024-04-17 12:24:22 +0000

    my_get_char_repeat v2

--- my_get_char_repeat.c
+++ my_get_char_repeat.c
@@ -1,14 +1,14 @@
 void print_char(char c);
 
 int my_get_char_repeat(char to_find, const char *to_search) {
-    int *len = 0;
+    int len = 0;
     int iter = 0;
-    while (to_find[len] != '\0') {
+    while (to_search[len] != '\0') {
         len++;
     }
-    for (int *i = 0; i < len; i++)
+    for (int i = 0; i < len; i++)
     {
-        if (to_find[i] == to_search) {
+        if (to_search[i] == to_find) {
             iter++;
         }
     }
